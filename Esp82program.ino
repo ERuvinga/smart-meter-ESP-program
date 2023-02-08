69156f92c60c65b6a7d063a8e9aad56bbea3a6a6
ERuvinga <ruvingaelie@gmail.com>
2023-02-08 23:16:02 +0200

    Setup Project

---
+++ Esp82program.ino
@@ -0,0 +1,11 @@
+// Program manager Esp Electronic To Water flow in Appartments
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
