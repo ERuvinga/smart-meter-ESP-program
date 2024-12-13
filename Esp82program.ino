c167805b971ada16de12f44bb42ee5b02e2a7030
ERuvinga <ruvingaelie@gmail.com>
2024-12-13 15:45:18 +0200

    Call datas to https protocol

--- Esp82program.ino
+++ Esp82program.ino
@@ -1,11 +1,40 @@
-// Program manager Esp Electronic To Water flow in Appartments
+/*
+   Esp Client
+*/
+#include "http.h"
+
+const char* ssid = "smart_M";
+const char* password = "SM_1234@";
+
+// define sensor datas
+void IRAM_ATTR count_01()
+{
+     Sensor_01.count();
+}
+void IRAM_ATTR count_02()
+{
+    Sensor_02.count();
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  // setup libs
+  Sensor_01.begin(count_01);
+  Sensor_02.begin(count_02);
+  
+  Serial.begin(57600);
+
+  WiFi.begin(ssid, password);
+  //Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    //Serial.print(".");
+  }
+//  Serial.println("");
+//  Serial.print("Connected to WiFi network with IP Address: ");
+//  Serial.println(WiFi.localIP());
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  SecureRequest();
 }
